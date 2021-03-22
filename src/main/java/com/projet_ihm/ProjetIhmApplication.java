69c4f8a970d5c3dfb52ea7ae725eb7756d3999d6
mac <Saad1130@>
2021-03-22 15:04:45 +0100

    Initial commit

---
+++ src/main/java/com/projet_ihm/ProjetIhmApplication.java
@@ -0,0 +1,13 @@
+package com.projet_ihm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetIhmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetIhmApplication.class, args);
+    }
+
+}
